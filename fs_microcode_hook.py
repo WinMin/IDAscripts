d4d347e963c0faf6fd35d30859e0559af45a9384
swing <bestswngs@gmail.com>
2025-04-14 21:47:56 +0800

    Update fs_microcode_hook.py
    Automatically batch decompile to obtain all related strings and string addresses

--- fs_microcode_hook.py
+++ fs_microcode_hook.py
@@ -1,14 +1,81 @@
 import ida_idaapi
 import ida_hexrays
 import ida_kernwin
+import ida_funcs
 import idaapi
 from binascii import unhexlify
+
+log_string_data = []
 
 def hex_to_signed_int32(hex_str):
     val = int(hex_str, 16)
     if val >= 0x80000000:
         val -= 0x100000000
     return val
+
+def decompile_all_functions():
+    if not ida_hexrays.init_hexrays_plugin():
+        print("Hex-Rays decompiler 未初始化或不可用。")
+        return
+
+    func_count = 0
+    for func in range(ida_funcs.get_func_qty()):
+        func_ea = ida_funcs.getn_func(func).start_ea
+        try:
+            cfunc = ida_hexrays.decompile(func_ea)
+            if cfunc is None:
+                # print(f"[x] 无法反编译函数: {hex(func_ea)}")
+                continue
+            # hook.func_printed(cfunc)
+            idaapi.open_pseudocode(func_ea, 0)
+            func_count += 1
+        except ida_hexrays.DecompilationFailure:
+            continue
+            #print(f"[!] 反编译失败: {hex(func_ea)}")
+        # print(f"\n完成，成功反编译了 {func_count} 个函数。")
+
+class FuncStringItem:
+    def __init__(self, ea, string):
+        self.ea = ea
+        self.string = string
+
+class FuncStringViewer(ida_kernwin.Choose):
+    def __init__(self,):
+        super().__init__(
+            "函数字符串列表",
+            [ ["函数地址", 20 | ida_kernwin.Choose.CHCOL_HEX], ["字符串内容", 80] ],
+            flags=ida_kernwin.Choose.CH_MULTI
+        )
+        # self.items = [FuncStringItem(ea, s) for ea, s in data_list]
+        self.items = []
+
+    def OnGetSize(self):
+        return len(self.items)
+
+    def OnGetLine(self, n):
+        item = self.items[n]
+        return [f"{item.ea:08X}", item.string]
+
+    def OnSelectLine(self, n):
+        if isinstance(n, list):
+            for idx in n:
+                ida_kernwin.jumpto(self.items[idx].ea)
+        else:
+            ida_kernwin.jumpto(self.items[n].ea)
+        return 1
+    
+    def feed(self, data):
+        """添加数据到视图中
+        data: 可以是单个元组 (ea, string) 或者包含多个元组的列表
+        """
+        if isinstance(data, tuple):
+            self.items.append(FuncStringItem(data[0], data[1]))
+        else:
+            for ea, s in data:
+                self.items.append(FuncStringItem(ea, s))
+        self.Refresh()
+        return
+
 
 class FSWriteHook(ida_hexrays.Hexrays_Hooks):
     def __init__(self):
@@ -109,10 +176,13 @@ class FSWriteHook(ida_hexrays.Hexrays_Hooks):
                 if b'\n' in strcpy_src_str:
                     strcpy_src_str = strcpy_src_str.replace(b'\n', b'\\n')
 
-                print(strcpy_src_str.decode('latin'))
+                
+                self._debug_print(f"strcpy_src_str: {strcpy_src_str.decode('latin')}")
+                self._debug_print(f"last_writfs_minsn: {hex(last_writfs_minsn.ea)}")
                 new_call = self._build_helper_strncpy(last_writfs_minsn, strcpy_src_str.decode('latin'))
                 last_blk.insert_into_block(new_call, last_writfs_minsn)
-
+                self._debug_print([last_writfs_minsn.ea, strcpy_src_str.decode('latin')])
+                viewer.feed((last_writfs_minsn.ea, strcpy_src_str.decode('latin')))
         return dirty
 
     def glbopt(self, mba):
@@ -142,6 +212,10 @@ class FSMicrocodePlugin(ida_idaapi.plugin_t):
             self.hook = FSWriteHook()
             try:
                 self.hook.hook()
+                decompile_all_functions() 
+                viewer.Show()
+     
+
             except Exception as e:
                 print(f"Error while hooking: {e}")
         else:
@@ -149,4 +223,6 @@ class FSMicrocodePlugin(ida_idaapi.plugin_t):
             return ida_idaapi.PLUGIN_SKIP
 
 def PLUGIN_ENTRY():
+    global viewer
+    viewer = FuncStringViewer()
     return FSMicrocodePlugin()
